42d746d011ed03f007fe9a82a1f98b78e6f84c02
shkeely <skhhackett@gmail.com>
2026-01-13 00:30:59 -0500

    Create client.ts

---
+++ src/types/client.ts
@@ -0,0 +1,15 @@
+import { createClient } from '@supabase/supabase-js';
+import type { Database } from './types';
+
+const SUPABASE_URL = 'https://bhtxgnqtnfvuehddkasg.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_FV9GdWPBR-SIzJk8bNGenQ_aFBLEccq';
+
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+  auth: {
+    storage: localStorage,
+    persistSession: true,
+    autoRefreshToken: true,
+  }
+});
+
+export const isSupabaseConfigured = true;
